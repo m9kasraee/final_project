affb0331fbf0337e02860f828af9039a9acf23d9
m9kasraee <m9kasraee@gmail.com>
2023-03-16 05:18:00 -0700

    icon added

--- index.js
+++ index.js
@@ -39,6 +39,15 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  document
+    .querySelector("#icon")
+    .setAttribute("alt", response.data.weather[0].main);
 }
 
 function searchCity(city) {
